0cb27a7fc1e24d64b7414bf7642fe0d3389b724b
oliver0524 <allabarry@aol.com>
2024-03-25 14:13:26 -0400

    Initial set up for Leave

--- src/main/java/org/capstone/entity/Leave.java
+++ src/main/java/org/capstone/entity/Leave.java
@@ -1,4 +1,27 @@
 package org.capstone.entity;
 
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+enum LeaveStatus {
+    APPROVED,
+    REJECTED,
+    PENDING
+}
+@Entity
 public class Leave {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int leaveId;
+    private String leaveName;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    //private LeaveStatus leaveStatus;
+    private boolean acceptRejectFlag;
+    private boolean active;
+    @ManyToOne
+    @JoinColumn(name = "employee_fk")
+    private List<SiteUser> siteUsers;
 }
