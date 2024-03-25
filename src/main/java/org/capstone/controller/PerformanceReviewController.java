7aa8212ce65fc2105568f35dc3a2e582d5d06255
NatalieHolly <natalieholly716@gmail.com>
2024-03-25 15:34:30 -0400

    GetAll Service/Controller methods

--- src/main/java/org/capstone/controller/PerformanceReviewController.java
+++ src/main/java/org/capstone/controller/PerformanceReviewController.java
@@ -1,9 +1,15 @@
 package org.capstone.controller;
 
+import org.capstone.entity.PerformanceReview;
 import org.capstone.service.PerformanceReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -14,5 +20,11 @@ public class PerformanceReviewController {
     public PerformanceReviewController(PerformanceReviewService performanceReviewService) {
         this.performanceReviewService = performanceReviewService;
     }
+
+    @GetMapping(value = "/performance")
+    public ResponseEntity<List<PerformanceReview>> getAllPerformanceReviews(){
+        List<PerformanceReview> pr = performanceReviewService.getAllPerformanceReviews();
+        return new ResponseEntity<>(pr, HttpStatus.OK);
+    }
 }
-//test comment
+
--- src/main/java/org/capstone/service/PerformanceReviewService.java
+++ src/main/java/org/capstone/service/PerformanceReviewService.java
@@ -1,8 +1,12 @@
 package org.capstone.service;
 
+import org.capstone.Main;
+import org.capstone.entity.PerformanceReview;
 import org.capstone.repository.PerformanceReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PerformanceReviewService {
@@ -11,5 +15,11 @@ public class PerformanceReviewService {
     @Autowired
     public PerformanceReviewService(PerformanceReviewRepository performanceReviewRepository) {
         this.performanceReviewRepository = performanceReviewRepository;
+    }
+
+    public List<PerformanceReview> getAllPerformanceReviews(){
+        Main.logger.info("logging method execution: PerformanceReviewService.getAllPerformanceReviews");
+        Main.logger.info("PerformanceReviewService.getAllPerformanceReviews: Performance Review list successfully retrieved.");
+        return performanceReviewRepository.findAll();
     }
 }
