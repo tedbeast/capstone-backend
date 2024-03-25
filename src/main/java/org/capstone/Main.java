f35b2d023980df82775a6f1ac9f5b30afd60eacf
tedbeast <theodore.balashov@revature.com>
2024-03-25 10:03:14 -0400

    init

---
+++ src/main/java/org/capstone/Main.java
@@ -0,0 +1,11 @@
+package org.capstone;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
