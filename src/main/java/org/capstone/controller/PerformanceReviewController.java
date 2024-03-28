a7e09ba0203b23ce92302e66380a506d3dbbfa5d
yagmurozkan2 <yagmurozkan292@gmail.com>
2024-03-28 16:13:31 -0400

    goal entity, updates to pr service, controller

--- src/main/java/org/capstone/controller/PerformanceReviewController.java
+++ src/main/java/org/capstone/controller/PerformanceReviewController.java
@@ -1,24 +1,13 @@
 package org.capstone.controller;
 
-import org.capstone.entity.Employee;
-import com.sun.net.httpserver.HttpsServer;
-import org.capstone.entity.PerformanceReview;
+import org.capstone.entity.*;
 import org.capstone.exception.PerformanceReviewException;
 import org.capstone.service.PerformanceReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-
 import org.springframework.web.bind.annotation.*;
-
-
 
 import java.util.*;
 
@@ -33,6 +22,7 @@ public class PerformanceReviewController {
         this.performanceReviewService = performanceReviewService;
     }
 
+    //returns all performance reviews for all employees
     @GetMapping("/performanceReview")
     public ResponseEntity<List<PerformanceReview>> getAllPerformanceReview(){
         List<PerformanceReview> performanceReviewList = performanceReviewService.getAllPerformanceReview();
@@ -46,11 +36,24 @@ public class PerformanceReviewController {
             return new ResponseEntity<>(employeeList, HttpStatus.OK);
     }
 
+    //returns all performance reviews for an employee
     @GetMapping("employee/{empId}/performanceReview")
     public ResponseEntity<List<PerformanceReview>> getAllPerformanceReview(@PathVariable int empId) {
         try{
             List<PerformanceReview> performanceReviewList = performanceReviewService.getAllPerformanceReviewsByEmployeeID(empId);
             return new ResponseEntity<>(performanceReviewList, HttpStatus.OK);
+        }
+        catch (PerformanceReviewException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //returns all goals for an employee
+    @GetMapping("employee/{empId}/goals")
+    public ResponseEntity<List<Goal>> getGoals(@PathVariable int empId) {
+        try{
+            List<Goal> goalList = performanceReviewService.getAllGoalsByEmployeeID(empId);
+            return new ResponseEntity<>(goalList, HttpStatus.OK);
         }
         catch (PerformanceReviewException e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -64,10 +67,10 @@ public class PerformanceReviewController {
 
     }
 
-    @PutMapping("employee/{empId}/performanceReview/{prId}/update")
-    public ResponseEntity<?> EmployeeComments(@RequestBody PerformanceReview p, @PathVariable("empId") int employeeID, @PathVariable("prId") int performanceReviewID) {
-        PerformanceReview performanceReview = performanceReviewService.employeeAddComments(employeeID, performanceReviewID, p);
-        return new ResponseEntity<PerformanceReview>(performanceReview, HttpStatus.OK);
+    @PutMapping("employee/{empId}/performanceReview/{prId}/{goalID}/update")
+    public ResponseEntity<?> EmployeeComments(@RequestBody Goal g, @PathVariable("empId") int employeeID, @PathVariable("prId") int performanceReviewID, @PathVariable("goalID") int goalID) {
+        Goal goal = performanceReviewService.employeeAddComments(employeeID, performanceReviewID, g, goalID);
+        return new ResponseEntity<>(goal, HttpStatus.OK);
     }
 
     @PutMapping("employee/{empId}/performanceReview/{prId}/managerReview")
@@ -76,11 +79,26 @@ public class PerformanceReviewController {
         return new ResponseEntity<PerformanceReview>(performanceReview, HttpStatus.OK);
     }
 
+    //posts a new performance review for an employee
     @PostMapping("/employee/{empId}/performanceReview")
-    public ResponseEntity<?> addPerformanceReview(@RequestBody PerformanceReview p, @PathVariable("empId") int employeeID) 
-        {
-            performanceReviewService.addPerformanceReview(employeeID, p);
-            return new ResponseEntity<PerformanceReview>(p, HttpStatus.OK);
+    public ResponseEntity<?> addPerformanceReview(@PathVariable("empId") int employeeID) {
+        try {
+            PerformanceReview performanceReview = performanceReviewService.addPerformanceReview(employeeID);
+            return new ResponseEntity<PerformanceReview>(performanceReview, HttpStatus.OK);
+        } catch (PerformanceReviewException e) {
+            return new ResponseEntity<>("Employee Not Found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("employee/{empId}/goals")
+    public ResponseEntity<?> addGoal(@RequestBody Goal g, @PathVariable("empId") int employeeID) throws PerformanceReviewException {
+        try {
+            Goal goal = performanceReviewService.addGoal(g, employeeID);
+            return new ResponseEntity<>(goal, HttpStatus.OK);
+        } catch (PerformanceReviewException e) {
+            return new ResponseEntity<>("Employee Not Found", HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
--- src/main/java/org/capstone/service/PerformanceReviewService.java
+++ src/main/java/org/capstone/service/PerformanceReviewService.java
@@ -13,16 +13,23 @@ import org.capstone.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.*;
 import java.util.*;
+
+import static java.time.temporal.TemporalAdjusters.lastDayOfYear;
 
 
 @Service
 public class PerformanceReviewService {
     PerformanceReviewRepository performanceReviewRepository;
+    GoalRepository goalRepository;
+    EmployeeRepository employeeRepository;
 
     @Autowired
-    public PerformanceReviewService(PerformanceReviewRepository performanceReviewRepository) {
+    public PerformanceReviewService(PerformanceReviewRepository performanceReviewRepository, GoalRepository goalRepository, EmployeeRepository employeeRepository) {
         this.performanceReviewRepository = performanceReviewRepository;
+        this.goalRepository = goalRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     public List<PerformanceReview> getAllPerformanceReview() {
@@ -35,16 +42,25 @@ public class PerformanceReviewService {
         return null; //performanceReviewRepository.findEmployeeByManagerID(managerID);
     }
 
+    public Employee checkIfEmployeeExistsByID(int employeeID) throws PerformanceReviewException {
+        Optional<Employee> optional = employeeRepository.findById(employeeID);
+        if (optional.isEmpty()) {
+            Main.logger.warn("Performance Review Get: Employee Not Found.");
+            throw new PerformanceReviewException("Employee Not Found.");
+        }
+        return optional.get();
+    }
+
     public List<PerformanceReview> getAllPerformanceReviewsByEmployeeID(int employeeID) throws PerformanceReviewException {
-        Main.logger.info("Performance Review Get: Attempting to get all performance reviews by Employee ID.");
-        List<PerformanceReview> performanceReview = performanceReviewRepository.findPerformanceReviewByEmployeeID(employeeID);
-        if(performanceReview.isEmpty()){
-            Main.logger.warn("Performance Review Get: Performance Review by Employee ID Not Found");
-            throw new PerformanceReviewException("Performance Review by Employee ID Not Found");
-        }
-        else {
-            return performanceReview;
-        }
+        Main.logger.info("Performance Review GET: Attempting to get all performance reviews by Employee ID.");
+        checkIfEmployeeExistsByID(employeeID);
+        return performanceReviewRepository.findPerformanceReviewByEmployeeID(employeeID);
+    }
+
+    public List<Goal> getAllGoalsByEmployeeID(int employeeID) throws PerformanceReviewException {
+        Main.logger.info("Goal GET: Attempting to get all goals by Employee ID.");
+        checkIfEmployeeExistsByID(employeeID);
+        return goalRepository.findGoalByEmployeeID(employeeID);
     }
 
     public List<PerformanceReview> getAllPerformanceByManager(int id){
@@ -53,15 +69,24 @@ public class PerformanceReviewService {
         return performanceReviewRepository.findPerformanceReviewByManagerID(id);
     }
 
-    public PerformanceReview employeeAddComments(int employeeID, int performanceReviewID, PerformanceReview p){
-        Optional<PerformanceReview> optional = performanceReviewRepository.findById(performanceReviewID);
+    public Goal employeeAddComments(int employeeID, int performanceReviewID, Goal g, int goalID){
+        Optional<Goal> optional = goalRepository.findById(goalID);
         if(optional.isEmpty()){
             //dothiserrorthing
         }
+        Goal goal = optional.get();
+        goal.setEmployeeComments(g.getEmployeeComments());
+        goalRepository.save(goal);
+        return goal;
+        /*
+        Optional<PerformanceReview> optional = performanceReviewRepository.findById(performanceReviewID);
+
         PerformanceReview performanceReview = optional.get();
+        performanceReview
         performanceReview.setEmployeeComments(p.employeeComments);
         performanceReviewRepository.save(performanceReview);
         return performanceReview;
+        */
     }
 
     public PerformanceReview managerAddComments(int employeeID, int performanceReviewID, PerformanceReview p){
@@ -75,9 +100,62 @@ public class PerformanceReviewService {
         return performanceReview;
     }
 
-    public PerformanceReview addPerformanceReview (int employeeID , PerformanceReview p) {
-      performanceReviewRepository.save(p);
-        return p;
+    public Date getLatestPerformanceReview(List<PerformanceReview> performanceReviewList) {
+        Date maxDate = performanceReviewList.get(0).getDeadlineDate();
+        for (int i = 1; i < performanceReviewList.size(); i++){
+            if (performanceReviewList.get(i).getDeadlineDate().after(maxDate)) {
+                maxDate = performanceReviewList.get(i).getDeadlineDate();
+            }
+        }
+        return maxDate;
     }
+
+    public PerformanceReview addPerformanceReview (int employeeID) throws PerformanceReviewException {
+        Main.logger.info("Performance Review POST: Attempting to create a new Performance Review for an employee.");
+        Employee employee = checkIfEmployeeExistsByID(employeeID);
+        PerformanceReview performanceReview = new PerformanceReview();
+        if (employee != null) {
+            List<PerformanceReview> performanceReviewList = getAllPerformanceReviewsByEmployeeID(employeeID);
+            if (performanceReviewList.isEmpty()) {
+                performanceReview.setDeadlineDate(java.sql.Date.valueOf(LocalDate.now().with(lastDayOfYear())));
+            }
+            else {
+                Date d = getLatestPerformanceReview(performanceReviewList);
+                Calendar c = Calendar.getInstance();
+                c.setTime(d);
+                c.add(Calendar.YEAR, 1);
+                performanceReview.setDeadlineDate(c.getTime());
+            }
+            performanceReview.setEmployee(employee);
+
+            performanceReviewList.add(performanceReview);
+            employee.setPerformanceReview(performanceReviewList);
+        }
+        return performanceReviewRepository.save(performanceReview);
+    }
+
+    public Goal addGoal(Goal goal, int employeeID) throws PerformanceReviewException {
+        Main.logger.info("Goal POST: Attempting to create a new goal for an employee.");
+        Employee employee = checkIfEmployeeExistsByID(employeeID);
+        if (employee != null) {
+            List<PerformanceReview> performanceReviewList = performanceReviewRepository.findPerformanceReviewByEmployeeID(employeeID);
+            if (performanceReviewList.isEmpty()) {
+                PerformanceReview newPerformanceReview = new PerformanceReview();
+                newPerformanceReview.setEmployee(employee);
+                newPerformanceReview.setDeadlineDate(java.sql.Date.valueOf(LocalDate.now().with(lastDayOfYear())));
+                performanceReviewRepository.save(newPerformanceReview);
+                performanceReviewList.add(newPerformanceReview);
+                employee.getPerformanceReview().add(newPerformanceReview);
+            }
+            List<Goal> goalList = goalRepository.findGoalByEmployeeID(employeeID);
+            PerformanceReview performanceReview = performanceReviewRepository.findPerformanceReviewByEmployeeIDandYear(employeeID, java.sql.Date.valueOf(LocalDate.now().with(lastDayOfYear())));
+            goalList.add(goal);
+            performanceReviewList.get(0).setGoals(goalList);
+            goal.setPerformanceReview(performanceReview);
+            goal.setPerformanceReview(performanceReviewList.get(0));
+        }
+        return goalRepository.save(goal);
+    }
+
 
 }
